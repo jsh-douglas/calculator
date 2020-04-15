e19cdfad04c5c392bd8b968872f6a081ed1f5ca0
Josh Douglas <46404670+jsh-douglas@users.noreply.github.com>
2020-04-15 12:52:16 +0100

    Updated Config Menu

--- calculator/script.js
+++ calculator/script.js
@@ -20,18 +20,34 @@ function del() {
 }
 
 function calculate() {
-    try {
-        calculation = calculation.replace(/\*\(/g, "(").replace(/\(/g, "*(").replace(/\+\*/g, "+").replace(/\-\*/g, "-").replace(/\/\*/g, "/");
-        document.getElementsByClassName("calc__output")[0].innerHTML = eval(calculation);
-    } catch {
-        document.getElementsByClassName("calc__output")[0].innerHTML = "Syntax Error";
+    if (calculation.length > 0) {
+        try {
+            calculation = calculation.replace(/\*\(/g, "(").replace(/\(/g, "*(").replace(/\+\*/g, "+").replace(/\-\*/g, "-").replace(/\/\*/g, "/");
+            document.getElementsByClassName("calc__output")[0].innerHTML = eval(calculation);
+        } catch {
+            document.getElementsByClassName("calc__output")[0].innerHTML = "Syntax Error";
+        }
+    }
+}
+
+function updateAccent(checkbox) {
+    if (checkbox.value == "red") {
+        document.documentElement.style.setProperty('--accent', '255, 54, 110');
+    } else if (checkbox.value == "blue") {
+        document.documentElement.style.setProperty('--accent', '82, 204, 255');
     }
 }
 
 function updateTheme(checkbox) {
-    if (checkbox.value == "red") {
-        document.documentElement.style.setProperty('--theme', '255, 54, 110');
-    } else if (checkbox.value == "blue") {
-        document.documentElement.style.setProperty('--theme', '82, 204, 255');
+    if (checkbox.value == "light") {
+        document.documentElement.style.setProperty('--theme', '255, 255, 255');
+        document.documentElement.style.setProperty('--text', '0, 0, 0');
+        document.documentElement.style.setProperty('--hover', '0, 0, 0, 0.025');
+        document.documentElement.style.setProperty('--shadow', '225, 225, 225');
+    } else if (checkbox.value == "dark") {
+        document.documentElement.style.setProperty('--theme', '40, 40, 40');
+        document.documentElement.style.setProperty('--text', '240, 240, 240');
+        document.documentElement.style.setProperty('--hover', '255, 255, 255, 0.05');
+        document.documentElement.style.setProperty('--shadow', '0, 0, 0');
     }
 }
